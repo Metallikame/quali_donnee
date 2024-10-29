9ed3a80aae31d5117953b570c7c9668d43960865
sayli <hessabilyas@gmail.com>
2024-10-29 11:00:21 +0100

    zerfv

--- src/component/NantesMap.js
+++ src/component/NantesMap.js
@@ -1,10 +1,43 @@
-import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import React, {useEffect, useState} from 'react';
+import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import axios from "axios";
+
+const icon = new L.Icon({
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [0, -41],
+});
 
 const NantesMap = () => {
     const center = [47.2184, -1.5536]; // Latitude et longitude de Nantes
     const zoomLevel = 13;
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const apiUrl = 'https://data.nantesmetropole.fr/api/explore/v2.1/catalog/datasets/244400404_tan-arrets/records'
+        axios.get(apiUrl)
+            .then(response => {
+                setData(response.data);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error('Une erreur s\'est produite', error);
+                setLoading(false);
+            });
+    }, []);
+
+    const points = [
+        {lat: 47.21393466, lng: -1.7230407, text: 'Audubon'},
+        {lat: 47.20917361, lng: -1.5500512, text: 'Aimé'},
+    ];
+
+
+
+    if (loading) return <p>Chargement...</p>;
 
     return (
         <MapContainer center={center} zoom={zoomLevel} style={{ height: "500px", width: "100%" }}>
@@ -12,6 +45,12 @@ const NantesMap = () => {
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
+
+            {points.map((point, index) => (
+                <Marker key={index} position={[point.lat, point.lng]} icon={icon}>
+                    <Popup>{point.text}</Popup>
+                </Marker>
+            ))}
         </MapContainer>
     );
 };
