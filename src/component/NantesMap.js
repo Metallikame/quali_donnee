34a00e35f026e9870569488157db64eff3503779
QuentinCOUET <qcouet49@gmail.com>
2024-10-31 16:17:02 +0100

    add json

--- src/component/NantesMap.js
+++ src/component/NantesMap.js
@@ -1,8 +1,7 @@
-import React, {useEffect, useState} from 'react';
-import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet';
+import React, { useEffect, useState } from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import axios from "axios";
 
 const icon = new L.Icon({
     iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
@@ -14,47 +13,26 @@ const icon = new L.Icon({
 const NantesMap = () => {
     const center = [47.2184, -1.5536]; // Latitude et longitude de Nantes
     const zoomLevel = 13;
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
+    const [stops, setStops] = useState([]);
+    const [trips, setTrips] = useState([]);
 
     useEffect(() => {
-        const fetchData = async () => {
-            let allData = [];
-            const limit = 100;
-            let offset = 0;
-            let hasMoreData = true;
-            let maxData = null;
-
-            while (hasMoreData) {
-                try {
-                    const response = await axios.get(
-                        `https://data.nantesmetropole.fr/api/explore/v2.1/catalog/datasets/244400404_tan-arrets/records?limit=${limit}&offset=${offset}`
-                    );
-
-                    if (!maxData) {
-                        maxData = response.data['total_count'];
-                    }
-
-                    const newData = response.data['results'].map(record => record);
-                    allData = [...allData, ...newData];
-
-                    offset += limit;
-                    if (allData.length >= maxData) hasMoreData = false;
-
-                } catch (error) {
-                    console.error('Une erreur s\'est produite', error);
-                    hasMoreData = false;
+        // Charger stops.json
+        fetch('/stops.json')
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
                 }
-            }
+                return response.json();
+            })
+            .then((data) => setStops(data))
+            .catch((error) => console.error("Erreur de chargement des données : ", error));
 
-            setData(allData);
-            setLoading(false);
-        };
-
-        fetchData();
+        // Charger trips.json
+        fetch('/trips.json')
+            .then((response) => response.json())
+            .then((data) => setTrips(data));
     }, []);
-
-    if (loading) return <p>Chargement...</p>;
 
     return (
         <MapContainer center={center} zoom={zoomLevel} className='w-full h-auto'>
@@ -63,9 +41,9 @@ const NantesMap = () => {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
 
-            {data.map((point, index) => (
-                <Marker key={index} position={[point.stop_coordinates.lat, point.stop_coordinates.lon]} icon={icon}>
-                    <Popup>{point.stop_name}</Popup>
+            {stops.map((stops, index) => (
+                <Marker key={index} position={[stops.stop_lat, stops.stop_lon]} icon={icon}>
+                    <Popup>{stops.stop_name}</Popup>
                 </Marker>
             ))}
         </MapContainer>
